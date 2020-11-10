17da3b291798c17a4bde1c0a6dbab5f14310d0db
Ricardo Maya <rickmaya@gmail.com>
2020-11-10 15:19:45 -0600

    add test files for snapshot test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React from 'react';
-function App() {
-  return (
-    <main className='App'>
-      {/* content goes here */}
-    </main>
-  );
+import React, {Component} from 'react';
+import Messages from './Messages';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>YOUR APPLICATION NAME!</h1>
+        <Messages name="Messages" unread={0}/>
+        <Messages name="Notifications" unread={10}/>
+      </div>
+    );
+  }
 }
+
 export default App;
