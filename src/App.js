1f3f5133dfb11eb18dc004c4ad2bfec78cdf956a
Ricardo Maya <rickmaya@gmail.com>
2020-11-20 14:08:32 -0600

    add new Counter component in state folder and import it and render in the App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Messages from './Messages';
 import TheDate from './state/TheDate';
+import Counter from './state/Counter';
 
 class App extends Component {
   render() {
@@ -10,6 +11,7 @@ class App extends Component {
         <Messages name="Messages" unread={0}/>
         <Messages name="Notifications" unread={10}/>
         <TheDate />
+        <Counter />
       </div>
     );
   }
