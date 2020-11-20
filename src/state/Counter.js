6a6603dbcbab75c3bc4a2d029a26d852dc12d69c
Ricardo Maya <rickmaya@gmail.com>
2020-11-20 14:15:19 -0600

    moved event handler to a method into a method on the class

--- src/state/Counter.js
+++ src/state/Counter.js
@@ -4,11 +4,14 @@ class Counter extends React.Component{
   state = { 
     count: 0
   };
+  handleButtonclick() {
+    console.log('clicked!')
+  }
   render() {
     return (
       <div>
         <p>The current count: {this.state.count}</p>
-        <button onClick={function() { console.log('clicked!') }}>
+        <button onClick={this.handleButtonclick}>
           Add 1
         </button>
       </div>
