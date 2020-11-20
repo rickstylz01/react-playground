86653a402bddf69c64bd693ad550383e80bae083
Ricardo Maya <rickmaya@gmail.com>
2020-11-20 13:29:47 -0600

    edit spelling of ReactDOM in test file

--- src/state/TheDate.test.js
+++ src/state/TheDate.test.js
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
 import TheDate from './TheDate';
 
-describe(`TheDate Component`, () => {
+describe('TheDate Component', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDom.render(<TheDate />, div);
+    ReactDOM.render(<TheDate />, div);
     ReactDOM.unmountComponentAtNode(div);
   })
 
   it.skip('renders the UI as expected', () => {
     expect(
-      renderer.create(<TheDate />,toJSON().toMatchSnapshot())
-    )
+      renderer.create(<TheDate />).toJSON()
+    ).toMatchSnapshot()
   })
 })
