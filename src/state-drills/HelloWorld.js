36533934fab9dc2b401b8d7f9f3a06977b669525
Ricardo Maya <rickmaya@gmail.com>
2020-11-23 02:08:34 -0600

    set state on onClick prop in the button element

--- src/state-drills/HelloWorld.js
+++ src/state-drills/HelloWorld.js
@@ -5,38 +5,39 @@ class HelloWorld extends React.Component {
     console.log('props in constructor', props)
     super(props)
     this.state = { 
-      who: "world"
+      who: "wrld"
     }
-  }
-  //refactor buttons later to make cleaner code DRY
-  handleWorldButtonclick = () => {
-    console.log('clicked', this.props)
-    this.setState({ 
-      who: "world"
-    })
-  }
-  handleFriendButtonclick = () => {
-    console.log('clicked', this.props)
-    this.setState({
-      who: "friend"
-    })
-  }
-  handleReactButtonclick = () => {
-    console.log('clicked', this.props)
-    this.setState({
-      who: "react"
-    })
   }
   render() {
     return (
       <div>
         <p>Hello, {this.state.who}</p>
-        <button onClick={this.handleWorldButtonclick}>World</button>
-        <button onClick={this.handleFriendButtonclick}>Friend</button>
-        <button onClick={this.handleReactButtonclick}>React</button>
+        <button onClick={() => this.setState({ who: 'world' })}>World</button>
+        <button onClick={() => this.setState({ who: 'friend' })}>Friend</button>
+        <button onClick={() => this.setState({ who: 'react' })}>React</button>
       </div>
     )
   }
 }
+  //refactor buttons later to make cleaner code DRY
+  // handleWorldButtonclick = () => {
+  //   console.log('clicked', this.props)
+  //   this.setState({ 
+  //     who: "world"
+  //   })
+  // }
+  // handleFriendButtonclick = () => {
+  //   console.log('clicked', this.props)
+  //   this.setState({
+  //     who: "friend"
+  //   })
+  // }
+  // handleReactButtonclick = () => {
+  //   console.log('clicked', this.props)
+  //   this.setState({
+  //     who: "react"
+  //   })
+  //}
+  
 
 export default HelloWorld
