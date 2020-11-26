ad7da60c0e8fe0d5be89c9fcafdb9f26cd23a9e3
Ricardo Maya <rickmaya@gmail.com>
2020-11-26 00:54:26 -0600

    set contructor initial state count to 0.  set interval in componeneDidMount for each second 1 is added to the count

--- src/state-drills/Bomb.js
+++ src/state-drills/Bomb.js
@@ -1,14 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 class Bomb extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { 
+      count: 0,
+    }
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({
+        count: this.count++
+      })
+    }, 1000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.interval)
   }
 
   render() {
     return (
-      <div>Hello, world</div>
+      <div>
+        <p>"tick"</p>
+      </div>
     )
   }
 
