2b9a9e9cf219e2df3cbafbea82cb8c290bbd40c2
Ricardo Maya <rickmaya@gmail.com>
2020-11-26 00:38:10 -0600

    initial commit

---
+++ src/state-drills/Bomb.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Bomb extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>Hello, world</div>
+    )
+  }
+
+}
+
+export default Bomb
