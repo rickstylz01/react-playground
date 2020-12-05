a6e401f8e694053ccf1499915831088fc4db743f
Ricardo Maya <rickmaya@gmail.com>
2020-12-04 18:14:01 -0600

    add if/else statement to display tick or tock or boom

--- src/state-drills/Bomb.js
+++ src/state-drills/Bomb.js
@@ -3,34 +3,43 @@ import React from 'react';
 class Bomb extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { 
-      count: 0,
-    }
+    this.state = {
+      count: 0
+    };
   }
 
   componentDidMount() {
     setInterval(() => {
       this.setState({
         count: this.count++
-      })
+      });
     }, 1000);
   }
+
+  displayTickorTock() {
+    const tick = <p>'tick'</p>;
+    const tock = <p>'tock'</p>;
+    const count = this.state.count;
+    let tickOrTock;
+
+    if (count % 2 === 0 && count < 8) {
+      tickOrTock = tick;
+    } else if (count % 2 !== 0 && count < 8) {
+      tickOrTock = tock;
+    } else {
+      tickOrTock = <p>'BOOM!!!!'</p>;
+    }
+    return tickOrTock;
+  }
+
+  render() {
+    console.log(this.count);
+    return <div>{this.displayTickorTock()}</div>;
+  }
+
   componentWillUnmount() {
     clearInterval(this.interval)
   }
-
-  // displayTickOrTock() {
-  //   //if count is divisible by 2 display 'tock'
-  // }
-
-  render() {
-    return (
-      <div>
-        <p>'tick'</p>
-      </div>
-    )
-  }
-
 }
 
 export default Bomb
