24a53682d09d84f86f1071c8d69f8e99babf5cc1
Ricardo Maya <rickmaya@gmail.com>
2020-12-07 14:04:04 -0600

    set the state as a callback in setInterval

--- src/state-drills/Bomb.js
+++ src/state-drills/Bomb.js
@@ -4,41 +4,40 @@ class Bomb extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      count: 0
+      count: 0,
     };
   }
 
   componentDidMount() {
-    setInterval(() => {
-      this.setState({
-        count: this.count++
-      });
+    this.interval = setInterval(() => {
+      this.setState((s) => ({
+        count: s.count + 1,
+      }));
     }, 1000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.interval);
   }
 
   displayTickorTock() {
+    const currentCount = this.state.count;
     const tick = <p>'tick'</p>;
     const tock = <p>'tock'</p>;
-    const count = this.state.count;
     let tickOrTock;
 
-    if (count % 2 === 0 && count < 8) {
+    if(currentCount >= 8) {
+      tickOrTock = <p>'BOOM!!!!'</p>
+    } else if (currentCount % 2 === 0) {
       tickOrTock = tick;
-    } else if (count % 2 !== 0 && count < 8) {
-      tickOrTock = tock;
     } else {
-      tickOrTock = <p>'BOOM!!!!'</p>;
+      tickOrTock = tock;
     }
     return tickOrTock;
   }
 
   render() {
-    console.log(this.count);
+    console.log(this.state.count);
     return <div>{this.displayTickorTock()}</div>;
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.interval)
   }
 }
 
