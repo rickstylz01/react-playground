a56f1b716532bd11effa15994d1f79b2466a0969
Ricardo Maya <rickmaya@gmail.com>
2020-12-07 22:05:14 -0600

    define handleButtonClick method

--- src/state-drills/RouletteGun.js
+++ src/state-drills/RouletteGun.js
@@ -1,24 +1,16 @@
 import React from 'react';
 
-export default class RouletteGun extends React.Component {
-  static defaultProps = {
-    bulletInChamber: 8,
-  };
-
-  state = {
+class RouletteGun extends React.Component {
+  state = { 
     chamber: null,
     spinningTheChamber: false,
-  };
-
-  componentWillUnmount() {
-    clearTimeout(this.timeout);
   }
 
-  handleButtonclick = () => {
-    this.setState({
-      spinningTheChamber: true,
-    });
-    
+  handleButtonClick = () => {
+    this.setState({ 
+      spinningTheChamber: true
+    })
+
     this.timeout = setTimeout(() => {
       const randomChamber = Math.ceil(Math.random() * 8);
 
@@ -27,17 +19,22 @@ export default class RouletteGun extends React.Component {
         spinningTheChamber: false,
       });
     }, 2000);
-    console.log(this.state);
   }
 
   render() {
     return (
       <div>
-        <p>spinning the chamber and pulling the trigger!...</p>
-        <button onClick={this.handleButtonclick}>
-          <p>Pull the trigger!</p>
+          <p>
+          spinning the chamber and pulling the trigger!...
+        </p>  
+        <button onClick={this.handleButtonClick()}>
+          <p>
+            Pull the trigger!
+          </p>
         </button>
       </div>
-    );
+    )
   }
 }
+
+export default RouletteGun
