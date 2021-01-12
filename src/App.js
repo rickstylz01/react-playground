49d5174516e17119aa2bddc4b44a4897bbad460a
Rick Maya <rickmaya@gmail.com>
2021-01-12 16:07:29 -0600

    create test for tab component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import React, {Component} from 'react';
-import Messages from './Messages';
-import TheDate from './state/TheDate';
-import Counter from './state/Counter';
+// import Messages from './Messages';
+// import TheDate from './state/TheDate';
+// import Counter from './state/Counter';
+import Tabs from './state-drills/Tabs';
+
 
 class App extends Component {
   render() {
+    const tabsProp = [
+      { name: 'First tab',
+      content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque.'},
+      { name: 'Second tab',
+      content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque.'},
+      { name: 'Third tab',
+      content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque.'},
+    ];
+
     return (
       <div className="App">
-        <h1>YOUR APPLICATION NAME!</h1>
-        <Messages name="Messages" unread={0}/>
-        <Messages name="Notifications" unread={10}/>
-        <TheDate />
-        <Counter count={123} step={5} />
+        {/*<h1>YOUR APPLICATION NAME!</h1>*/}
+        {/*<Messages name="Messages" unread={0}/>*/}
+        {/*<Messages name="Notifications" unread={10}/>*/}
+        {/*<TheDate />*/}
+        {/*<Counter count={123} step={5} />*/}
+
+        <Tabs tabs={tabsProp}/>
       </div>
     );
   }
